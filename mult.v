f2d184eb7f13a3fc4bebcd31c2272201cd7e58ee
David L Samuels <dls808@scarecrow.ece.northwestern.edu>
2017-02-08 17:51:36 -0600

    finished multiplier

--- mult.v
+++ mult.v
@@ -1,23 +1,31 @@
-`define width 32
+`define width 34
 `timescale 1ns/1ps
 
-module booth_mult (p, x, y);
+module booth_mult (p, a, b, clk, sign);
     parameter width=`width;
     parameter N = `width/2;
-    input[width-1:0]x, y;
-    output[width+width-1:0]p;
+    input[width-3:0]a, b;
+    input clk, sign;
+    output[width+width-5:0]p;
+    reg out;
+    wire [width-1:0] x, y; 
     
+    assign x = {{a[31], a[31]} & {sign, sign} , a};
+    assign y = {{b[31], b[31]} & {sign, sign} , b};
     reg [2:0] cc[N-1:0];
     reg [width:0] pp[N-1:0];
     reg [width+width-1:0] spp[N-1:0];
     reg [width+width-1:0] prod;
     wire [width:0] inv_x;
-    integer kk, ii;
+    integer kk, ii, jj;
+    integer count;
 
     assign inv_x = {~x[width-1],~x}+1;
 
-    always @ (x or y or inv_x)
+    always @ (x or y or inv_x or sign)
     begin
+        //$display("x is %x, y is %x", x, y);
+        count = 0;
         cc[0] = {y[1], y[0], 1'b0};
         for(kk=1;kk<N;kk=kk+1)
             cc[kk] = {y[2*kk+1],y[2*kk],y[2*kk-1]};
@@ -35,8 +43,20 @@ module booth_mult (p, x, y);
                 spp[kk] = {spp[kk],2'b00}; //multiply by 2 to the power x or shifting operation
         end //for(kk=0;kk<N;kk=kk+1)
         prod = spp[0];
-        for(kk=1;kk<N;kk=kk+1)
-        prod = prod + spp[kk];
     end
-    assign p = prod;
+ 
+    always @ (posedge clk)
+    begin
+        case(count)
+            0: for(jj=1;jj<N/4;jj=jj+1)  prod = prod + spp[jj];
+            1: for(jj=N/4;jj<N/2;jj=jj+1)  prod = prod + spp[jj];
+            2: for(jj=N/2;jj<3*N/4;jj=jj+1)  prod = prod + spp[jj];
+            3: for(jj=3*N/4;jj<N;jj=jj+1)  begin prod = prod + spp[jj];  end    
+            default: ;
+        endcase
+        //for(kk=1;kk<N;kk=kk+1)
+        //prod = prod + spp[kk];
+        count = (count + 1);
+    end
+    assign p = prod[width*2-5:0];
 endmodule
--- tests/mult_test.v
+++ tests/mult_test.v
@@ -2,22 +2,36 @@ module mult_test();
     reg [31:0] A;
     reg [31:0] B;  
     wire [63:0] result;
+    reg sign;
+    reg clk;
 
     booth_mult MULT_32 (
         .p(result),
-        .x(A),
-        .y(B));
+        .a(A),
+        .b(B),
+        .clk(clk),
+        .sign(sign));
 
 integer i; reg[63:0] answer; 
 
     initial begin
-//        $monitor("A=%x, B=%x, result=%x", A, B, result);
-        #0 A=$random%32; B=$random%32;
-        for(i = 0; i < 1000; i=i+1)
+        $monitor("A=%x, B=%x, sign = %b, result=%x", A, B,sign, result);
+        #0 A=$random%32; B=$random%32; clk = 0; sign = 1;
+        for(i = 0; i < 10; i=i+1)
         begin
-            #1 answer = $signed(A)*$signed(B);
+            #4 answer = $signed(A)*$signed(B);
                if (result != answer) $display("ERROR result should be %x, is %x", answer, result);
-            #1 A=$random%32; B=$random%32;
+            #0 A=$random%32; B=$random%32;
+        end
+        #10 sign = 0;
+        for(i = 0; i < 10; i=i+1)
+        begin
+            #8 answer = A*B;
+               if (result != answer) $display("ERROR result should be %x, is %x", answer, result);
+            #0 A=$random%32; B=$random%32;
         end
     end
+
+    always
+        #1 clk = ~clk;
 endmodule
