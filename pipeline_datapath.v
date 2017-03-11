49dc387e23eb2c5f9433c91bdb121839d1dd437b
wdw133 <williamwallace2018@u.northwestern.edu>
2017-03-10 20:34:35 -0600

    added multiply lock

--- pipeline_datapath.v
+++ pipeline_datapath.v
@@ -13,7 +13,7 @@ module pipeline_datapath (input clk, output [0:31] instr);
     wire [0:4] write_reg_wb, write_reg_id, write_reg_ex, write_reg_mem, regA_id, regB_id; 
     wire [0:8] ctrl_id, ctrl_ex, ctrl_mem;
     wire [0:2] dmem_info_id, dmem_info_ex, dmem_info_mem;
-     
+    wire reg_lock_mult; 
     //should be wires once connected
     wire reg_lock_if, reg_lock_if1;
     reg reg_lock_id, reg_lock_ex, reg_lock_mem, reg_lock_wb;
@@ -30,7 +30,7 @@ module pipeline_datapath (input clk, output [0:31] instr);
 
 
 
-    i_fetch I_FETCH(.clk(clk),.reg_lock(reg_lock_if), .target(target_id), .jump_or_branch(jump_or_branch_id), .instr(instruction), .pc_plus_four(pc_plus_four_if));
+    i_fetch I_FETCH(.clk(clk),.reg_lock(reg_lock_if), .target(target_id), .jump_or_branch(jump_or_branch_id), .instr(instruction), .pc_plus_four(pc_plus_four_if), .reg_lock_mult(reg_lock_mult));
    
     assign reg_lock_if = reg_lock_if1;
     i_decode I_DECODE(
@@ -39,7 +39,7 @@ module pipeline_datapath (input clk, output [0:31] instr);
         .WriteReg(write_reg_wb), .WriteData(write_data_wb), .pc_plus_four(pc_plus_four_if),
         .write_reg_ex(write_reg_id), .write_reg_mem(write_reg_ex), .write_val_ex(alu_non_reg_ex),
         .write_val_mem(alu_out_ex), .fp_we(fp_reg_write_wb), .fp_write_ex(fp_reg_write_id),
-        .fp_write_mem(fp_reg_write_ex),
+        .fp_write_mem(fp_reg_write_ex), .reg_lock_mult(reg_lock_mult),
         //outputs
         .ctrl_reg(ctrl_id), .alu_ctrl_reg(alu_ctrl_id), .busA_reg(busA_id),.busB_reg(busB_id), 
         .imm_ext_reg(imm_ext_id), .dmem_info_reg(dmem_info_id),.jump_or_branch(jump_or_branch_id) , 
@@ -59,13 +59,13 @@ module pipeline_datapath (input clk, output [0:31] instr);
         //outputs
         .ctrl_reg(ctrl_ex), .alu_out_reg(alu_out_ex), .write_data_reg(write_mem_ex), 
         .dmem_info_reg(dmem_info_ex), .write_reg_reg(write_reg_ex), .mult_out_reg(mult_out_ex), 
-        .alu_ctrl_reg(alu_ctrl_ex), .alu_out(alu_non_reg_ex), .fp_reg_write(fp_reg_write_ex));
+        .alu_ctrl_reg(alu_ctrl_ex), .alu_out(alu_non_reg_ex), .fp_reg_write(fp_reg_write_ex), .reg_lock_mult(reg_lock_mult));
 
     mem_stage MEM_STAGE (
         //inputs
         .clk(clk), .reg_lock(reg_lock_mem), .ctrl(ctrl_ex), .alu_out(alu_out_ex), .write_data(write_mem_ex),
         .dmem_info(dmem_info_ex), .write_reg(write_reg_ex), .mult_out(mult_out_ex), .alu_ctrl(alu_ctrl_ex),
-        .fp_write(fp_reg_write_ex),
+        .fp_write(fp_reg_write_ex), .reg_lock_mult(reg_lock_mult),
         //outputs
         .ctrl_reg(ctrl_mem), .mem_out_reg(mem_out_mem), .alu_out_reg(alu_out_mem), 
         .dmem_info_reg(dmem_info_mem),.write_reg_reg(write_reg_mem), .fp_reg_write(fp_reg_write_mem));
